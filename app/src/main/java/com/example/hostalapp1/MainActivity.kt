bdfaa6235a75cdc2dd09412ca58cd3ab3ceb90e0
Bastykey <169182698+Bastykey@users.noreply.github.com>
2026-01-17 19:20:44 -0300

    Commit 3: Agregando vista de listado de hostales.

--- app/src/main/java/com/example/hostalapp1/MainActivity.kt
+++ app/src/main/java/com/example/hostalapp1/MainActivity.kt
@@ -14,7 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -43,6 +43,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(modifier: Modifier = Modifier) {
 
     val context = LocalContext.current
+    var showHostales by remember { mutableStateOf(false) }
 
     Column(
         modifier = modifier
@@ -60,14 +61,14 @@ fun Greeting(modifier: Modifier = Modifier) {
         Spacer(modifier = Modifier.height(24.dp))
 
         Button(onClick = {
-            Toast.makeText(context, "Ver hostales", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, "Ingresando...", Toast.LENGTH_SHORT).show()
         }) {
-            Text("Ingresar ")
+            Text("Ingresar")
         }
 
         Spacer(modifier = Modifier.height(32.dp))
-        // operadores crud
-        Text(text = "¿ Que Vamos a Hacer ?")
+
+        Text(text = "¿Qué vamos a hacer?")
 
         Spacer(modifier = Modifier.height(16.dp))
 
@@ -80,12 +81,26 @@ fun Greeting(modifier: Modifier = Modifier) {
         Spacer(modifier = Modifier.height(8.dp))
 
         Button(onClick = {
-            Toast.makeText(context, "Leer Hostales", Toast.LENGTH_SHORT).show()
+            showHostales = true
         }) {
-            Text("Leer Hostales")
+            Text("Ver Hostales")
         }
 
-        Spacer(modifier = Modifier.height(8.dp))
+        if (showHostales) {
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(text = "Hostales Disponibles", fontSize = 20.sp)
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(text = "Hostal Plaza Centro - $25.000")
+            Text(text = "Hostal Santiago - $37.000")
+            Text(text = "Hostal Costanera - $43.000")
+            Text(text = "Hostal Vitacura - $55.000")
+            Text(text = "Hostal Las Condes - $79.000")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
 
         Button(onClick = {
             Toast.makeText(context, "Actualizar Hostal", Toast.LENGTH_SHORT).show()
