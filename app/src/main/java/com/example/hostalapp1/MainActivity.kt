3dd0652002154571e4687ccedae1f9e0ff187f31
Bastykey <169182698+Bastykey@users.noreply.github.com>
2026-01-17 18:47:11 -0300

    Commit 2: Creando diseño de pantalla principal

--- app/src/main/java/com/example/hostalapp1/MainActivity.kt
+++ app/src/main/java/com/example/hostalapp1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.hostalapp1
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -16,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -39,6 +41,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
+
+    val context = LocalContext.current
+
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -46,6 +51,7 @@ fun Greeting(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
         Text(
             text = "Bienvenido a HostalApp",
             fontSize = 24.sp
@@ -53,8 +59,46 @@ fun Greeting(modifier: Modifier = Modifier) {
 
         Spacer(modifier = Modifier.height(24.dp))
 
-        Button(onClick = { }) {
-            Text(text = "Ver Hostales")
+        Button(onClick = {
+            Toast.makeText(context, "Ver hostales", Toast.LENGTH_SHORT).show()
+        }) {
+            Text("Ingresar ")
+        }
+
+        Spacer(modifier = Modifier.height(32.dp))
+        // operadores crud
+        Text(text = "¿ Que Vamos a Hacer ?")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            Toast.makeText(context, "Crear Hostal", Toast.LENGTH_SHORT).show()
+        }) {
+            Text("Crear Hostal")
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Button(onClick = {
+            Toast.makeText(context, "Leer Hostales", Toast.LENGTH_SHORT).show()
+        }) {
+            Text("Leer Hostales")
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Button(onClick = {
+            Toast.makeText(context, "Actualizar Hostal", Toast.LENGTH_SHORT).show()
+        }) {
+            Text("Actualizar Hostal")
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Button(onClick = {
+            Toast.makeText(context, "Eliminar Hostal", Toast.LENGTH_SHORT).show()
+        }) {
+            Text("Eliminar Hostal")
         }
     }
 }
